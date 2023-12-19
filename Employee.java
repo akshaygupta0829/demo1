7cfbb811b8a957ccf3d81ad62e8a36bf23062be6
akshaygupta0829 <117994110+akshaygupta0829@users.noreply.github.com>
2023-12-19 15:44:24 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,8 @@
+package day1;
+//This example illustrates the use of Class and Object but it lacks Data Hiding and therefore there is no Abstraction and Encapsulation
+public class Employee {
+	//ID (Integer), Name (Character Based), Salary (Fraction)
+	int employeeId;
+	String employeeName;//String is a pre-defined class in Java. It is not a data type.
+	float employeeSalary;
+}
